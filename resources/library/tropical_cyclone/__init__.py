2f862d080799a47f06d92000f8bcfa4109959925
Dewid-d <davidedonno97@gmail.com>
2025-11-21 09:44:44 +0100

    -DD : update source files header

--- resources/library/tropical_cyclone/__init__.py
+++ resources/library/tropical_cyclone/__init__.py
@@ -1,7 +1,7 @@
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 # ByteStorm Library for Tropical Cyclone Tracking           #
 # Copyright (c) 2025 CMCC Foundation                        #
-# Licensed under The MIT License [see LICENSE for details]  #
+# Licensed under The GPLv3 License [see LICENSE for details]#
 # Written by Davide Donno                                   #
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 
